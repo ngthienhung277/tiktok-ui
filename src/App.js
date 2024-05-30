a2d25d79a40859f5364deb73b36cfc54800682fc
Nguyen Thien Hung <ngthienhung277@gmail.com>
2024-05-30 16:01:51 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
-
+import { useState } from 'react';
 function App() {
+  const [name, setName] = useState(``)
+
+  console.log(name);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{padding: 32}}>
+      <input
+      value={name}
+      onChange={ e => setName(e.target.value)}/>
     </div>
   );
 }
