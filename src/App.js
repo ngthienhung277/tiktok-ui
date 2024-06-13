a8dbc5093d3c896d29ed022aa9c4a7e4def2d8c2
Nguyen Thien Hung <ngthienhung277@gmail.com>
2024-06-13 16:07:34 +0700

    relaunch project

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+
 function App() {
-  const [name, setName] = useState(``)
-
-  console.log(name);
-
   return (
-    <div style={{padding: 32}}>
-      <input
-      value={name}
-      onChange={ e => setName(e.target.value)}/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
