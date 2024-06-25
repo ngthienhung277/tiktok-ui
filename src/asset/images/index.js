b5c5e3a20bb02abc5a13c063f7ae29e274154478
Nguyen Thien Hung <ngthienhung277@gmail.com>
2024-06-25 13:41:34 +0700

    Header #6

--- src/asset/images/index.js
+++
@@ -1,5 +0,0 @@
-const images =  {
-    logo: require('~/asset/images/logo.svg').default
-}
-
-export default images
--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -1,18 +1,19 @@
 import classNames from 'classnames/bind';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {faCircleQuestion,faCircleXmark ,faCoins,faEarthAsia,faEllipsisVertical,faGear,faKeyboard,faMagnifyingGlass,faSpinner, faCloudUpload, faUser, faSignOut } from '@fortawesome/free-solid-svg-icons';
+import {faCircleQuestion,faCircleXmark ,faCoins,faEarthAsia,faEllipsisVertical,faGear,faKeyboard,faSpinner, faUser, faSignOut } from '@fortawesome/free-solid-svg-icons';
 
 import Tippy from '@tippyjs/react';
 import Button from '~/components/Button'
 import { Wrapper as PopperWrapper} from '~/components/Popper'
 import styles from './Header.module.scss';
-import images from '~/asset/images';
+import images from '~/assets/images';
 import { useEffect, useState } from 'react';
 import AccountItem from '~/components/AccountItem';
 import HeadlessTippy from '@tippyjs/react/headless';
 import 'tippy.js/dist/tippy.css';
 import Menu from '~/components/Popper/Menu';
-
+import { UploadIcon, MessageIcon, SearchIcon, InboxIcon } from '~/components/Icons';
+import Image from '~/components/Image';
 const cx = classNames.bind(styles);
 
 const MENU_ITEMS = [
@@ -51,7 +52,7 @@ function Header() {
     
     useEffect(() => {
         setTimeout(() => {
-            setSearchResult([])
+            setSearchResult([1, 2, 3])
         }, 3000)
     },[]);
 
@@ -121,7 +122,7 @@ function Header() {
                         <FontAwesomeIcon className={cx('loading')} icon={faSpinner} />
 
                         <button className={cx('search-btn')}>
-                            <FontAwesomeIcon icon={faMagnifyingGlass} />
+                            <SearchIcon/>
                         </button>
                     </div>
                 </HeadlessTippy>
@@ -129,10 +130,21 @@ function Header() {
                 <div className={cx('actions')}>
                     {currentUser ? (
                         <>
-                            <Tippy delay={[0, 200]} content= "Upload video" placement='bottom'>
+                            <Tippy delay={[0, 50]} content= "Upload video" placement='bottom'>
                                     <button className={cx('action-btn')}>
-                                        <FontAwesomeIcon icon={faCloudUpload}/>
+                                        <UploadIcon/>
                                     </button>
+                            </Tippy>
+                            <Tippy delay={[0, 50]} content="Message" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <MessageIcon />
+                                </button>
+                            </Tippy>
+                            <Tippy delay={[0, 50]} content="Inbox" placement="bottom">
+                                <button className={cx('action-btn')}>
+                                    <InboxIcon />
+                                    <span className={cx('badge')}>12</span>
+                                </button>
                             </Tippy>
                         </>
                     ) : (
@@ -143,9 +155,9 @@ function Header() {
                         )}
                             <Menu items= {currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
                             {currentUser ? (
-                                <img
+                                <Image
                                     className={cx('user-avatar')}
-                                    src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/ea0854578085ab26effc2c7b8cefa270~c5_100x100.jpeg?x-expires=1651658400&x-signature=zeUCDyTxctGYZ5%2Bsh422klviXFE%3D"
+                                    src="https://files.fullstack.edu.vn/f8-prod/user_avatars/1/623d4b2d95cec.png"
                                     alt="Nguyen Van A"
                                 />
                                 ):(
